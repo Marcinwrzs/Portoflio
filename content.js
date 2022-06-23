008c27bb3c883ce7135eaf745190600c2d8df96b
Marcinwrzs <77743373+Marcinwrzs@users.noreply.github.com>
2022-06-23 16:18:56 +0200

    Updated description

---
+++ content.js
@@ -0,0 +1,101 @@
+export const about = `
+<div class="photo">
+</div>
+<div class="content">
+  <h1>About Me!</h1>
+
+  <h2>Marcin Wrzoś</h2>
+  <p>In my current role I am responsible for the process improvement by writing macros in VBA. During this task I have found out the passion in programming and creating applications, which resulted in the decision to change my career path to IT. At this moment I am able to write applications in Javascript using webpack, preprocessors like Sass and expand my knowledge by learning React. Working for over 3 years in investment banking taught me to work under pressure and practice my soft skills during the meetings with external clients.</p>
+  <div>
+    <i class="fa-solid fa-phone"></i>
+    <a href="tel:+48 662 623 531">+48 662 623 531</a>
+  </div>
+  <div>
+    <i class="fa-solid fa-envelope"></i>
+    <a href="mailto: marcin.wrzs@gmail.com">Marcin.wrzs@gmail.com</a>
+  </div>
+</div>
+`;
+
+export const work = `
+<h1>Work</h1>
+
+<h3>Factor Law</h3>
+<p>Analyst, Senior Financial Services Operations</p>
+<ul>
+  <li>Improving the process by VBA</li>
+  <li>Working with external client</li>
+  <li>Coordinating the work allocation across the Negotiator teams
+    based in Europe and North America</li>
+</ul>
+
+<h3>Credit Suisse</h3>
+<p>Security and Pricing Data Management Specialist</p>
+<ul>
+  <li>
+    Maintaining data of financial instruments and their prices for all business
+  </li>
+  <li>
+    Delivering the security and pricing data
+  </li>
+</ul>
+
+<h3>UBS</h3>
+<p>Data & Document Specialist</p>
+<ul>
+  <li>
+    Providing customer service and support to Client Advisors and Front Office
+  </li>
+  <li>
+    Capturing client static data based on documentation
+  </li>
+  <li>
+    Contributing to continuous improvement of processes and services
+  </li>
+</ul>
+
+<h3>BNY Mellon</h3>
+<p>Junior Representative, Reconciliation</p>
+<ul>
+  <li>
+    Investigating the breaks and fails
+  </li>
+  <li>
+    Cash or position reconciliation against broker/custodian statements
+  </li>
+  <li>
+    Processing trade amendment, dividends and verification
+  </li>
+</ul>
+`;
+
+export const education = `
+<h1>Education</h1>
+
+<div>
+  <p>10.2017 – 07.2019 Wroclaw University of Economics</p>
+  <p>The Faculty of Management and Computer Science</p>
+  <p>Finance and Accounting</p>
+</div>
+
+<div>
+  <p>10.2014 – 07.2017 University of Wroclaw</p>
+  <p>Faculty of Social Sciences</p>
+  <p>National Security</p>
+</div>
+`;
+
+
+export const projects = `
+<div class="photo">
+</div>
+<div class="content">
+  <h1>Projects</h1>
+
+  <h2>Blackjack</h2>
+  <a href="https://github.com/Marcinwrzs/Blackjack.git" target="_blank">View Project!</a>
+  <p>Project has been written in the <b>Object Oriented Programming</b> concept and bundled in the
+    <b>Webpack</b></p>
+  <p>Technologies which have been used: <b>HTML, SCSS, Javascript</b></p>
+</div>
+`;
